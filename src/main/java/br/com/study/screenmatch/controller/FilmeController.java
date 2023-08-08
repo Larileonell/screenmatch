e04b87620e905d6a78010626ffed4957898918ec
Allana Larissa <larissaleonel98@gmail.com>
2023-08-08 16:25:18 -0300

    Add getters and setters

--- src/main/java/br/com/study/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/study/screenmatch/controller/FilmeController.java
@@ -1,14 +1,20 @@
 package br.com.study.screenmatch.controller;
 
 import br.com.study.screenmatch.domain.filmes.DadosCadastroFilmes;
+import br.com.study.screenmatch.domain.filmes.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
+
+    private List<Filme> filmes = new ArrayList<>();
 
     @GetMapping
     public String carregaPaginaFormulario(){
@@ -16,8 +22,8 @@ public class FilmeController {
     }
     @PostMapping
     public String cadastraFilme(DadosCadastroFilmes dados){
-        System.out.println(dados);
-
+       var filme = new Filme(dados);
+       filmes.add(filme);
         return "filmes/formulario";
     }
 }
