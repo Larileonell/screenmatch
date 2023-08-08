5c5d16cea86a543ec5c59d4e41cc3faf6d792533
Allana Larissa <larissaleonel98@gmail.com>
2023-08-08 16:11:14 -0300

    Add Formulario and PostingMapping

--- src/main/java/br/com/study/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/study/screenmatch/controller/FilmeController.java
@@ -1,7 +1,9 @@
 package br.com.study.screenmatch.controller;
 
+import br.com.study.screenmatch.domain.filmes.DadosCadastroFilmes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,12 @@ public class FilmeController {
     @GetMapping
     public String carregaPaginaFormulario(){
     return "filmes/formulario";
+    }
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilmes dados){
+        System.out.println(dados);
+
+        return "filmes/formulario";
     }
 }
 
---
+++ src/main/java/br/com/study/screenmatch/domain/filmes/DadosCadastroFilmes.java
@@ -0,0 +1,4 @@
+package br.com.study.screenmatch.domain.filmes;
+
+public record DadosCadastroFilmes(String nome, Integer duracao, Integer ano, String genero) {
+}
