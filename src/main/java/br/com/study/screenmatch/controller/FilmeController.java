afdbe10579d7bae35eb6b3dbf7c3fb2d2a068e69
Allana Larissa <larissaleonel98@gmail.com>
2023-08-08 15:18:12 -0300

    Initial commit

---
+++ src/main/java/br/com/study/screenmatch/controller/FilmeController.java
@@ -0,0 +1,9 @@
+package br.com.study.screenmatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filmes")
+public class FilmeController {
+}
